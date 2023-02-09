06ce5cba2acff70302b145add8bb2f553ad65551
Selim Talha Aksoy <alha@pop-os.localdomain>
2023-02-09 23:23:02 +0300

    first commit

---
+++ components/allPageComponent/navbar.tsx
@@ -0,0 +1,52 @@
+export default function Navbar(){
+    return(
+        <nav className="bg-gray-700 text-purple-400 w-min h-screen overflow-y-scroll overflow-x-hidden flex flex-col justify-start items-center px-6 pt-2 shadow-md">
+            <div className="border-b-2 border-gray-100 pb-2 flex mb-2">
+                <span className=" font-JetBrainsMono text-4xl text-white">Alha</span>
+                <span className=" font-JetBrainsMono text-4xl text-purple-500">UI</span>
+            </div>
+            <fieldset className="flex flex-col border-2 border-gray-400 rounded-md w-60 pb-2 mb-2">
+                <legend className="pl-2">Actions</legend>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Button</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Dropdown</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Modal</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Swap</span>
+            </fieldset>
+            <fieldset className="flex flex-col border-2 border-gray-400 rounded-md w-60 pb-2 mb-2">
+                <legend className="pl-2">Actions</legend>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Button</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Dropdown</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Modal</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Swap</span>
+            </fieldset>
+            <fieldset className="flex flex-col border-2 border-gray-400 rounded-md w-60 pb-2 mb-2">
+                <legend className="pl-2">Actions</legend>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Button</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Dropdown</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Modal</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Swap</span>
+            </fieldset>
+            <fieldset className="flex flex-col border-2 border-gray-400 rounded-md w-60 pb-2 mb-2">
+                <legend className="pl-2">Actions</legend>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Button</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Dropdown</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Modal</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Swap</span>
+            </fieldset>
+            <fieldset className="flex flex-col border-2 border-gray-400 rounded-md w-60 pb-2 mb-2">
+                <legend className="pl-2">Actions</legend>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Button</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Dropdown</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Modal</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Swap</span>
+            </fieldset>
+            <fieldset className="flex flex-col border-2 border-gray-400 rounded-md w-60 pb-2 mb-2">
+                <legend className="pl-2">Actions</legend>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Button</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Dropdown</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Modal</span>
+                <span className=" w-full py-2 rounded-md pl-4 transition-all duration-300 hover:cursor-pointer hover:bg-slate-800/30 hover:pl-6">Swap</span>
+            </fieldset>
+        </nav>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/allPageComponent/navbar'
+import {AiOutlineCaretDown} from '@react-icons/all-files/ai/AiOutlineCaretDown'
 
 const inter = Inter({ subsets: ['latin'] })
+
+function Flow()
+{
+  function WhyIUse(){
+    return(
+      <div id="usage" className='w-full h-screen flex flex-row flex-wrap bg-red-500'>
+      </div>
+    );
+  }
+  return(
+    <div className='w-full h-screen overflow-scroll bg-black'>
+      <div className='w-full h-screen bg-pattern flex flex-col gap-y-2 justify-center items-center'>
+        <span className='text-9xl'>Alha UI</span>
+        <span>Tailwind Based UI Library</span>
+        <span className='flex flex-row justify-center items-center'>Learn More &nbsp; <a href="#usage"><AiOutlineCaretDown className='text-2xl hover:cursor-pointer'/></a> </span>
+      </div>
+      <WhyIUse></WhyIUse>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
@@ -14,109 +35,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <main className='w-full min-h-screen bg-slate-200 flex'>
+        <Navbar></Navbar>
+        <Flow></Flow>
       </main>
     </>
   )
